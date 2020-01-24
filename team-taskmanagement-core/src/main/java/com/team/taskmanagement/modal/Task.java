79a57ebe7adc1b8054908f96e43c2957ebe77b2f
saarmann <kaspar.saarmann@gmail.com>
2020-01-24 16:34:55 +0200

    Create classes: user, task

---
+++ team-taskmanagement-core/src/main/java/com/team/taskmanagement/modal/Task.java
@@ -0,0 +1,17 @@
+package com.team.taskmanagement.modal;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Task {
+
+    private Date taskDate;
+    private Time startTime;
+    private Time endTime;
+    private String customerName;
+    private String taskDescription;
+    private String user;
+    private Time timeSpent;
+    private double hoursSpent;
+
+}
---
+++ team-taskmanagement-core/src/main/java/com/team/taskmanagement/modal/User.java
@@ -0,0 +1,12 @@
+package com.team.taskmanagement.modal;
+
+public class User {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+
+}
