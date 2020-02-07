e46c66f659a08423685353cecc02642657cf755f
saarmann <kaspar.saarmann@gmail.com>
2020-02-07 16:45:09 +0200

    Invoice model

--- team-taskmanagement-core/src/main/java/com/team/taskmanagement/model/Invoice.java
+++ team-taskmanagement-core/src/main/java/com/team/taskmanagement/model/Invoice.java
@@ -1,19 +1,37 @@
 package com.team.taskmanagement.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.sql.Date;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Data
 public class Invoice {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    @Column (unique = true)
     private String invoiceNumber;
+
+    @Temporal(TemporalType.DATE)
     private Date invoiceDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date dueDate;
+
+    private double invoiceAmount;
+
+    @Column (columnDefinition = "integer default 0")
+    private int invoiceStatus;
+
+    @Column (columnDefinition = "integer default 0")
+    private int invoiceSent;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId")
+    private Customer customer;
 
 }
